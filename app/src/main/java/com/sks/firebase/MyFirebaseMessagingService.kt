29a9b565e8e9bf0df625c1b9e3c5b9caa23fa687
shubham5i2 <shubhamkumarsingh450@gmail.com1>
2024-02-17 20:09:21 +0530

    added push notification

---
+++ app/src/main/java/com/sks/firebase/MyFirebaseMessagingService.kt
@@ -0,0 +1,12 @@
+package com.sks.firebase
+
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+    }
+
+}
